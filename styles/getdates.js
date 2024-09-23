9b7b94884b3b97d520885d8d02d90b05fffdec64
yrrab911 <barry.leidy@gmail.com>
2024-09-23 14:54:11 -0600

    completed W02 Assignment: Picture Album

--- styles/getdates.js
+++ styles/getdates.js
@@ -9,6 +9,20 @@ function displayLastModified() {
     document.getElementById('lastModified').textContent = 'Last Modified: ' + lastModified; 
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('nav');
 
+    hamburger.addEventListener('click', function() {
+        nav.classList.toggle('active'); 
+
+       
+        if (nav.classList.contains('active')) {
+            hamburger.textContent = '✖'; 
+        } else {
+            hamburger.textContent = '☰'; 
+        }
+    });
+});
 displayCurrentYear();
 displayLastModified();
